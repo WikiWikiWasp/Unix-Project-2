a42e4cf0e541f43b7ff70e0e6cb1274ec16b8bbc
Mark McDermott <mcdermottsolutions@gmail.com>
2017-04-25 10:21:12 -0500

    adding comments to mycat.c

--- mycat.c
+++ mycat.c
@@ -11,28 +11,29 @@ int main(int argc, char *argv[]) {
   struct stat pathStat;
   FILE *fp;
 
-  if (argc > 1) {
+  if (argc > 1) {                  // if arguments are supplied, cat each one
     int i=1;
-    for (;i<argc; i++) {
+    for (;i<argc; i++) {           // loop through the args
       filename = argv[i];
-      stat(filename, &pathStat);
-      fp = fopen(filename, "r");
-      if (fp == NULL) {
+      stat(filename, &pathStat);   // get file info
+      fp = fopen(filename, "r");   // open file
+      if (fp == NULL) {            // throw error if not found
         printf("mycat: %s: No such file or directory",filename);
         exit(1);
       }
-      if (S_ISDIR(pathStat.st_mode)) {
-        printf("mycat: %s: Is a directory\n",filename);
+      if (S_ISDIR(pathStat.st_mode)) {                  // if dir, not file
+        printf("mycat: %s: Is a directory\n",filename); // print "is a dir"
       }
-      while (fgets(buffer, BUFSIZ, fp) != NULL) {
+      while (fgets(buffer, BUFSIZ, fp) != NULL)         // print file contents
         printf("%s",buffer);
       };
     }
   } else {
+      // if no args detected, handle the cases ./mycat <foo.txt & mycat $(myls)
       while (read(STDIN_FILENO, &ch, 1) != 0) {
-        if (ch == ' ') {
+        if (ch == ' ') {   // manually add line breaks for mycat $(myls) case
           printf("\n");
-        } else {
+        } else {           // print each char in case of ./mycat <foo.txt
           printf("%c",ch);
         }
       }
